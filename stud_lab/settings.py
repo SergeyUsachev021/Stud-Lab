2682b42dd3b384d3cd38a07365661efbd8499bca
feni <7feeni@gmail.com>
2026-04-28 21:45:54 +0700

    Добавил DRF, djoser и JWT аутентификацию

--- stud_lab/settings.py
+++ stud_lab/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'djoser',
+
     'apps.users'
 ]
 
@@ -91,6 +94,12 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 
 
 # Password validation
@@ -128,3 +137,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'users.User'
