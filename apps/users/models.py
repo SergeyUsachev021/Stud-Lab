3304c73ab43acf691872977dbdb734034a3195fb
Egorka <egor.rychko.22@mail.ru>
2026-05-16 20:17:14 +0700

    Добавил SellerProfile и BuyerProfile.

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 class User(AbstractUser):
     yandex_id = models.BigIntegerField(blank=True, null=True)
@@ -13,8 +14,19 @@ class BaseProfile(models.Model):
     class Meta:
         abstract = True
 
+
 class SellerProfile(BaseProfile):
-    pass
+    rating = models.DecimalField(max_digits=3, decimal_places=2, default=0.00)
+    total_sales = models.IntegerField(default=0)
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Seller: {self.name}"
+
 
 class BuyerProfile(BaseProfile):
-    pass
+    total_purchases = models.IntegerField(default=0)
+    favorite_categories = models.JSONField(default=list, blank=True)
+
+    def __str__(self):
+        return f"Buyer: {self.name}"
